db298979c8ef84e145a9c8a7f7afda5776d43c01
PipocaPorto <jpportocampos@gmail.com>
2023-01-12 12:24:29 -0300

    corrigindo regra de endereco e adicionando application.properties basico

--- src/main/java/io/github/jpportocampos/domain/entity/Pessoa.java
+++ src/main/java/io/github/jpportocampos/domain/entity/Pessoa.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -26,5 +27,5 @@ public class Pessoa {
     private LocalDate dataNascimento;
 
     @OneToOne(mappedBy = "pessoa")
-    private Endereco endereco;
+    private List<Endereco> endereco;
 }
